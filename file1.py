314f7d01f1d337e59468fbb8350da2592375ea6f
hayato898 <aei.jnr.tx.159.357@icloud.com>
2025-12-01 12:48:05 +0900

    feature：file1.pyの変更

--- file1.py
+++ file1.py
@@ -2,10 +2,11 @@ from random import randint as rd
 
 def roll_dice():
     while True:
-        dice = rd(1, 6)
-        if dice == 6:
+        dice_1 = rd(1, 6)
+        dice_2 = rd(1, 6)
+        if dice_1 == dice_2:
             break
-    return dice
+    return dice_1
 
 if __name__ == "__main__":
     result = roll_dice()
